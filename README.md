eae05f2e2b722be3746243f8f92a82eb1a3ad5ff
Hugh Kennedy <hughskennedy@gmail.com>
2013-10-29 12:37:18 +1100

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# preload [![experimental](http://hughsk.github.io/stability-badges/dist/experimental.svg)](http://github.com/hughsk/stability-badges) #
+
+Load images, scripts and CSS with XHR progress events where supported.
+
+This has been written more for large, script/asset-heavy pages such as games
+or WebGL demos where the page is already going to take 5+ seconds to load.
+As such, it's not as small as it could be - I'd recommend
+[LazyLoad](https://github.com/rgrove/lazyload) if you're looking for
+a lightweight solution.
+
+## Usage ##
+
+[![preload](https://nodei.co/npm/preload.png?mini=true)](https://nodei.co/npm/preload)
+
+Work in progress.
+
+## License ##
+
+MIT. See [LICENSE.md](http://github.com/hughsk/preload/blob/master/LICENSE.md) for details.
---
+++ index.js
@@ -0,0 +1,121 @@
+var insertCss = require('insert-css')
+var EventEmitter = require('events').EventEmitter
+var inherits = require('inherits')
+var once = require('once')
+var xhr = require('xhr')
+
+module.exports = Preload
+
+inherits(Preload, EventEmitter)
+function Preload() {
+  if (!(this instanceof Preload)) return new Preload
+  EventEmitter.call(this)
+  this.targets = {}
+  this.targetCount = 0
+}
+
+var proto = Preload.prototype
+
+proto.js = function js(src, callback) {
+  callback = callback || throwop
+  if (Array.isArray(src)) return many(this, 'js', src, callback)
+
+  var self = this
+  watch(this, src, xhr(src, function(err, res, body) {
+    if (err) return callback(err)
+    var script = document.createElement('script')
+    self.progress(src, self.targets[src] = 1)
+    script.setAttribute('src', 'data:text/javascript;charset=UTF-8,' + encodeURIComponent(body))
+    document.body.appendChild(script)
+    callback()
+  }))
+
+  return this
+}
+
+proto.css = function css(src, callback) {
+  callback = callback || throwop
+  if (Array.isArray(src)) return many(this, 'css', src, callback)
+
+  var self = this
+  watch(this, src, xhr(src, function(err, res, body) {
+    if (err) return callback(err)
+    self.progress(src, self.targets[src] = 1)
+    insertCss(body)
+  }))
+
+  return this
+}
+
+proto.img = function img(src, callback) {
+  callback = callback || throwop
+  if (Array.isArray(src)) return many(this, 'img', src, callback)
+
+  var self = this
+  if (!('Blob' in window)) {
+    var img = document.createElement('img')
+    self.targets[src] = 0
+    self.targetCount++
+    img.onload = function() {
+      self.progress(src, self.targets[src] = 1)
+      callback()
+    }
+    img.src = src
+  } else {
+    var req = watch(this, src, xhr(src, function(err, res, body) {
+      if (err) return callback(err)
+      self.progress(src, self.targets[src] = 1)
+    }))
+
+    req.responseType = 'blob'
+  }
+
+  return this
+}
+
+proto.progress = function progress(src, progress) {
+  var total = 0
+  for (var key in this.targets) {
+    if (this.targets.hasOwnProperty(key)) {
+      total += this.targets[key] / this.targetCount
+    }
+  }
+
+  this.emit('progress', src, progress, total)
+
+  return this
+}
+
+function watch(self, src, req) {
+  self.targets[src] = 0
+  self.targetCount++
+
+  req.onprogress = function(e) {
+    var value = e.lengthComputable ? e.loaded / e.total : 0
+    self.targets[src] = value
+    self.progress(src, value)
+  }
+
+  return req
+}
+
+function many(self, key, values, callback) {
+  callback = once(callback)
+
+  var responses = []
+  var counter = values.length
+
+  for (var i = 0; i < counter; i += 1) (function(i) {
+    self[key](values[i], function(err, res) {
+      if (err) return callback(err)
+      responses[i] = res
+      if (!counter--) callback(null, values)
+    })
+  })(i)
+
+  return self
+}
+
+function throwop(err) {
+  if (err) throw err
+}
